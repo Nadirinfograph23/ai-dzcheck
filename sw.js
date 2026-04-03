746fb684208bf5bd249c91340a4b3db63ec8689c
Nadirinfograph23 <nadirfortest44@gmail.com>
2026-04-03 06:43:59 +0100

    restore: الكود الأصلي من الفرع الأصلي

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Offline support & caching
    ============================================ */
 
-var CACHE_NAME = 'aidzcheck-v25';
+var CACHE_NAME = 'aidzcheck-v21';
 var ASSETS = [
     '/',
     '/index.html',
